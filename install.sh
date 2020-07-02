03687a02c8345ba720e6fbe9db736ba21321e193
Diego Alves <alvessd@live.com>
2020-07-01 23:53:06 -0300

    add install configs

--- install.sh
+++ install.sh
@@ -2,7 +2,21 @@
 
 installation() {
   sudo apt update
-  sudo apt install build-essential git curl wget vim neofetch htop nmap tmux neovim openssh-server
+  sudo apt install curl
+  sudo apt install build-essential
+  sudo apt install wget
+  sudo add-apt-repository ppa:git-core/ppa
+  sudo apt update
+  sudo apt install git
+  sudo apt install vim
+  sudo apt install neofetch
+  sudo apt install htop
+  sudo apt install nmap
+  sudo apt ubuntu-restricted-extras
+  sudo apt install tmux
+  #sudo apt install neovim
+  #sudo apt install openssh-server
+  sudo apt update
   sudo apt upgrade
   echo "Installation completed"
 }
@@ -11,10 +25,31 @@ symlinks() {
   cp .gitconfig $HOME/.gitconfig
   cp .bashrc $HOME/.bashrc
   cp .bash_aliases $HOME/.bash_aliases
+  cp .vim $HOME -r
+  cp .vimrc $HOME/.vimrc
   echo "Symlinks files copied"
 }
 
+languages() {
+  curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+  sudo apt install nodejs
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  sudo apt update
+  sudo apt install yarn
+
+  wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb && sudo dpkg -i erlang-solutions_2.0_all.deb
+  sudo apt update
+  sudo apt install esl-erlang
+  sudo apt install elixir
+
+  sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+  sudo apt install clang
+
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
+
 installation
+languages
 symlinks
 
 source ~/.bashrc
