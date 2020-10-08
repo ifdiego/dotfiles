80a161e9aef5116c0b5951e75f7a1e02ff1e93be
Diego Alves <alvessd@live.com>
2020-10-07 22:05:37 -0300

    removing programming languages

--- install.sh
+++ install.sh
@@ -27,17 +27,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 clear
 vim -c "PlugInstall" -c "q" -c "q"
 
-echo "Installing programming languages"
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb && sudo dpkg -i erlang-solutions_2.0_all.deb
-sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
-
-sudo apt update
-sudo apt install nodejs \
-  yarn esl-erlang elixir clang
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
 sudo apt update
 sudo apt upgrade
 
