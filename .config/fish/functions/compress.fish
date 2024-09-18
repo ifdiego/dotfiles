0ae1affd73bab96527c4d701215958167007c870
Diego Alves <alvessd@live.com>
2024-09-17 21:03:36 -0300

    remove more unused

--- .config/fish/functions/compress.fish
+++
@@ -1,3 +0,0 @@
-function compress --description "compress a video using ffmpeg"
-    ffmpeg -i $argv -vcodec libx265 -crf 28 compressed_$argv
-end
--- .config/fish/functions/ignore.fish
+++
@@ -1,3 +0,0 @@
-function ignore --description "generate gitignore files"
-    curl -fLw "\n" https://www.gitignore.io/api/$argv
-end
