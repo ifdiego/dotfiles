dc4a71c8a31a441d38497338f82c39b1fff3749e
Diego Alves <ifdiego@users.noreply.github.com>
2025-03-08 22:21:55 -0300

    feat: small updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,16 +6,13 @@ abbr --add tree "eza --tree --color=never"
 abbr --add ls "eza"
 abbr --add ll "eza --long"
 abbr --add la "eza --long --all"
-abbr --add ta "tmux new -A -s default"
+abbr --add ta "tmux new -A -s 0"
+abbr --add cat "bat"
+abbr --add task "go-task"
 
 fish_add_path (go env GOPATH)/bin
 fish_add_path ~/.cargo/bin
 
-set --global fish_greeting
-set --global fish_key_bindings fish_vi_key_bindings
-
-set --global --export EDITOR nvim
-set --global --export GPG_TTY (tty)
 set --global --export MANPAGER nvim +Man!
 
 function sesh-sessions
@@ -28,16 +25,12 @@ function sesh-sessions
     commandline -f repaint
 end
 
-bind --mode insert \es sesh-sessions
+bind --mode default \ck sesh-sessions
+
+fish_config prompt choose arrow
 
 fzf --fish | source
-export FZF_CTRL_T_OPTS="
-    --walker-skip .git,node_modules,target
-    --preview 'bat -n --color=always {}'
-    --bind 'ctrl-/:change-preview-window(down|hidden|)'
-    --bind 'ctrl-o:execute($EDITOR {} &> /dev/tty)'"
-
-starship init fish | source
+go-task --completion fish | source
 zoxide init fish | source
 
 if test (tty) = "/dev/tty1"
