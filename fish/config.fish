3f3629c202a62b9352e059e66a4e9ddf89c5733e
Diego Alves <ifdiego@users.noreply.github.com>
2025-07-06 17:48:50 -0300

    feat: updating packages

--- fish/config.fish
+++ fish/config.fish
@@ -2,14 +2,17 @@ set fish_greeting
 set EDITOR nvim
 set MANPAGER less -Rc
 
-abbr --add g "git"
-abbr --add v "nvim"
+abbr --add g git
+abbr --add v nvim
 abbr --add temp "pushd (mktemp -d)"
 abbr --add ta "tmux new -A -s 0"
 abbr --add ks "tmux kill-server"
-abbr --add task "go-task"
-abbr --add vim "nvim"
+abbr --add task go-task
+abbr --add vim nvim
 abbr --add vimdiff "nvim -d"
+abbr --add hx helix
+abbr --add lg lazygit
+abbr --add zj zellij
 
 fish_add_path (go env GOPATH)/bin
 fish_add_path ~/.cargo/bin
